1ce0df82d675115dac78b97b782cd577ea517dce
vzvu3k6k <vzvu3k6k@gmail.com>
2019-09-21 04:58:19 +0900

    Use lograge

--- additional_environment.rb
+++ additional_environment.rb
@@ -2,4 +2,8 @@ if ENV["RAILS_LOG_TO_STDOUT"].present?
   logger           = ActiveSupport::Logger.new(STDOUT)
   logger.formatter = config.log_formatter
   config.logger = ActiveSupport::TaggedLogging.new(logger)
+
+  config.lograge.enabled = true
+  config.lograge.keep_original_rails_log = true
+  config.lograge.logger = logger
 end
